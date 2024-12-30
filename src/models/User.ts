0eebbbc45445419339191468c36f1f5e60785179
Mateusz Karpiński <111981363+MateuszKar2@users.noreply.github.com>
2024-12-30 19:10:13 +0100

    add file

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,4 +1,9 @@
 import { Model } from "./Model";
+import { Attributes } from './Attributes';
+import { ApiSync } from "./ApiSync";
+import { Eventing } from './Eventing';
+import { Collection } from "./Collection";
+import { json } from "stream/consumers";
 
 
 export interface UserProps {
@@ -10,9 +15,19 @@ export interface UserProps {
 const rootUrl = 'http://localhost:3000/users';
 
 export class User extends Model<UserProps> {
+    static buildUser(attrs: UserProps): User {
+        return new User(
+           new Attributes<UserProps>(attrs),
+           new Eventing(),
+           new ApiSync<UserProps>(rootUrl)
+        );
+    }
 
+    static buildUserCollection(): Collection<User, UserProps> {
+        return new Collection<User, UserProps>(
+            rootUrl,
+            (json: UserProps) => User.buildUser(json)
+        );
+    }
+}
 
-
-};
-
-   
